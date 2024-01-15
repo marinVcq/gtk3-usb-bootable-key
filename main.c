fb261164fa085c63f6016d7afbf10c981a5212ad
Marin Durand <marinvacquier@hotmail.fr>
2024-01-15 22:20:16 +0100

    Minor Front & Dynamic usb list retrieve

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 typedef struct {
     GtkBuilder *builder;
     
+    // Variable to store the selected USB device
+    gchar *selected_usb_device;
+    
     // Container
     GtkGrid *main_grid;
     GtkGrid *format_grid;
@@ -20,7 +23,6 @@ typedef struct {
     
     GtkLabel *header_label;
     GtkComboBoxText *combobox_usb;
-    GtkComboBoxText *combobox_iso;
     GtkComboBoxText *combobox_format;
     GtkLabel *label_format_information;
     GtkLabel *label_bootable_information;
@@ -30,6 +32,10 @@ typedef struct {
     GtkButton *create_bootable_button;
 
 } App;
+
+void on_combobox_usb_changed(GtkComboBoxText *combobox, App *app);
+gboolean check_for_usb_changes(gpointer user_data);
+void populate_usb_devices(GtkComboBoxText *combobox, App *app);
 
 gboolean simulate_formating(gpointer user_data) {
     App *app = (App *)user_data;
@@ -83,7 +89,42 @@ void on_format_button_clicked(GtkButton *button, gpointer user_data) {
     g_timeout_add_seconds(1, simulate_formating, app);
 }
 
-void populate_usb_devices(GtkComboBoxText *combobox) {
+// Signal handler for USB device selection change
+void on_combobox_usb_changed(GtkComboBoxText *combobox, App *app) {
+
+    // Get the selected USB device
+    const gchar *selected_device = gtk_combo_box_text_get_active_text(combobox);
+    
+    if(selected_device != NULL){
+		// Free the previously selected USB device
+		g_free(app->selected_usb_device);
+		// Store the selected USB device in the App structure
+		app->selected_usb_device = g_strdup(selected_device);    
+    }
+
+    // Print the selected USB device to the console
+    g_print("Selected USB device: %s\n", app->selected_usb_device);
+}
+
+// New function to dynamically update the USB devices list
+void populate_usb_devices_and_update(App *app) {
+    // Check for USB changes and update the list
+    populate_usb_devices(app->combobox_usb, app);
+}
+
+// Periodic function to check for USB changes
+gboolean check_for_usb_changes(gpointer user_data) {
+    App *app = (App *)user_data;
+
+    // Check for USB changes and update the list
+    populate_usb_devices_and_update(app);
+
+    // Returning TRUE allows the timeout to run periodically
+    return TRUE;
+}
+
+// Modified populate_usb_devices function
+void populate_usb_devices(GtkComboBoxText *combobox, App *app) {
     FILE *lsblk_output = popen("lsblk -I 8 -dnpo NAME,TRAN", "r");
     if (lsblk_output == NULL) {
         g_printerr("Error opening lsblk command\n");
@@ -95,7 +136,13 @@ void populate_usb_devices(GtkComboBoxText *combobox) {
 
     // Clear existing items
     gtk_combo_box_text_remove_all(combobox);
-    gtk_combo_box_text_insert_text(combobox, 0, "Select USB Device");
+    
+    if(app->selected_usb_device ==NULL){
+    	gtk_combo_box_text_insert_text(combobox, 0, "Select USB Device");
+    }else{
+        gtk_combo_box_text_insert_text(combobox, 0, app->selected_usb_device);
+    }
+
 
     while (fgets(buffer, sizeof(buffer), lsblk_output) != NULL) {
         gchar *device_name, *transport_type;
@@ -116,8 +163,14 @@ void populate_usb_devices(GtkComboBoxText *combobox) {
         gtk_combo_box_text_append_text(combobox, "No USB device detected");
     }
 
-    // Set the first item as active
+ 
     gtk_combo_box_set_active(GTK_COMBO_BOX(combobox), 0);
+
+    // You can set up a periodic check for USB changes using g_timeout_add
+    if(app->selected_usb_device == NULL){
+    	g_timeout_add_seconds(5, check_for_usb_changes, app);
+    }
+    
 }
 
 // Initialize the application structure
@@ -133,20 +186,21 @@ App *create_app(GtkBuilder *builder) {
     app->header_label = GTK_LABEL(gtk_builder_get_object(builder, "header_label"));
     app->header_image = GTK_IMAGE(gtk_builder_get_object(builder, "header_image"));
     app->combobox_usb = GTK_COMBO_BOX_TEXT(gtk_builder_get_object(builder, "combobox_usb"));
-    app->combobox_iso = GTK_COMBO_BOX_TEXT(gtk_builder_get_object(builder, "combobox_iso"));
     app->combobox_format = GTK_COMBO_BOX_TEXT(gtk_builder_get_object(builder, "combobox_format"));
     gtk_combo_box_text_insert_text(GTK_COMBO_BOX_TEXT(app->combobox_format), 0, "Select Format");
-    gtk_combo_box_text_insert_text(GTK_COMBO_BOX_TEXT(app->combobox_iso), 0, "Select ISO");
     gtk_combo_box_text_insert_text(GTK_COMBO_BOX_TEXT(app->combobox_format), 1, "NTFS");
     gtk_combo_box_text_insert_text(GTK_COMBO_BOX_TEXT(app->combobox_format), 2, "FAT32");
     gtk_combo_box_set_active(GTK_COMBO_BOX(app->combobox_format), 0);
-    gtk_combo_box_set_active(GTK_COMBO_BOX(app->combobox_iso), 0);
     app->label_format_information = GTK_LABEL(gtk_builder_get_object(builder, "label_format_information"));
+    
+    // Initialize the selected USB device to NULL
+    app->selected_usb_device = NULL;
 
     return app;
 }
 
 void destroy_app(App *app) {
+	g_free(app->selected_usb_device);
     g_free(app);
 }
 
@@ -193,10 +247,11 @@ int main(int argc, char *argv[]) {
     
 
     // Populate USB devices in the combo box
-    populate_usb_devices(app->combobox_usb);
+    populate_usb_devices(app->combobox_usb, app);
 
     // Button Format event
     g_signal_connect(app->format_button, "clicked", G_CALLBACK(on_format_button_clicked), app);
+    g_signal_connect(app->combobox_usb, "changed", G_CALLBACK(on_combobox_usb_changed), app);
     app->label_format_information = GTK_LABEL(gtk_builder_get_object(builder, "label_format_information"));
 
 
